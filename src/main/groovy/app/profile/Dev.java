b649d018ed0aad4d3503c4e16205340cdf9a368a
Tolkv <lavcraft@gmail.com>
2015-01-14 16:50:54 +0300

    Profiles mistake :)

---
+++ src/main/groovy/app/profile/Dev.java
@@ -0,0 +1,17 @@
+package app.profile;
+
+import org.springframework.context.annotation.Profile;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by tolkv on 1/14/2015.
+ */
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Profile("dev")
+public @interface Dev {
+}
